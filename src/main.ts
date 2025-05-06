ffb603d402a98a787f6f3eb953a58462e0e66c43
SulayGisel <sulaymartinez939@gmail.com>
2025-05-05 21:51:38 -0500

    se cambia el main

--- src/main.ts
+++ src/main.ts
@@ -11,48 +11,17 @@ async function bootstrap() {
   // Configure CORS before other middleware
   app.enableCors({
     // Allow multiple origins
-    origin: [
-      'http://localhost:4200',
-      'https://front-horas-five.vercel.app',
-      'https://backhorasextra-production.up.railway.app',
-    ],
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
-    allowedHeaders: [
-      'Content-Type',
-      'Authorization',
-      'Origin',
-      'X-Requested-With',
-      'Accept',
-    ],
-    credentials: true,
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  });
+      origin: [
+        'http://localhost:4200',
+        'https://front-horas-five.vercel.app',
+        'https://backhorasextra-production.up.railway.app',
+      ],
+      methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+      credentials: true,
+    });
 
-  // 👉 Sesiones habilitadas
-  app.use(
-    session({
-      secret: 'my-secret', // Usa una clave secreta segura en producción
-      resave: false,
-      saveUninitialized: false,
-      cookie: {
-        maxAge: 1000 * 60 * 60, // 1 hora
-        secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', // Important for cross-site requests
-      },
-    }),
-  );
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true, //transforma automaticamente los datos
-    }),
-  );
 
-  const reflector = app.get(Reflector);
-  app.useGlobalGuards(new JwtAuthGuard(reflector));
 
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
